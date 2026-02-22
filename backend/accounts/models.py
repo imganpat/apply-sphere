aeca0fd828bee0b2e035867e1131b9e90e1e6ffc
Ganpat <pganpatchoudhary@gmail.com>
2026-02-22 22:39:30 +0530

    Add custom user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,4 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-    
+
+class User(AbstractUser):
+    username = None  
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=150)
+
+    is_verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.email
