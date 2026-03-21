42eaf163c9fcb467364e8ee180495afc17ab4487
Ganpat <pganpatchoudhary@gmail.com>
2026-03-21 21:32:04 +0530

    Create api endpoints

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen text-2xl font-bold">
-      ApplySphere 🚀
-    </div>
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-3xl font-bold">ApplySphere 🚀</h1>
+      <Button>Click Me</Button>
+    </div> 
   );
 }
---
+++ frontend/src/lib/api.js
@@ -0,0 +1,76 @@
+const BASE_URL = "http://localhost:8000/";
+
+const getToken = () => localStorage.getItem("access_token");
+
+const request = async (endpoint, options= {}) => {
+    const headers = {
+        "Content-Type": "application/json",
+        ...(getToken() && {Authorization: `Bearer ${getToken()}`}),
+        ...options.headers,
+    }
+
+    const res = await fetch(`${BASE_URL}${endpoint}`, {
+        ...options,
+        headers,
+    });
+
+    if(res.status === 401) {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        window.location.href = "/api/login/";
+        return;
+    }
+
+    if(res.status === 204) {
+        return null; // No content
+    }
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw new Error(data?.detail || JSON.stringify(data) || "Something went wrong");
+    }
+
+    return data;
+}
+
+
+// Auth
+
+export const registerUser = (userData) => {
+    request("api/register/", {
+        method: "POST",
+        body: JSON.stringify(userData),
+    });
+}
+
+export const loginUser = (credentials) => {
+    return request("api/login/", {
+        method: "POST",
+        body: JSON.stringify(credentials),
+    });
+}
+
+export const refreshToken = (refreshToken) => {
+    return request("api/token/refresh/", {
+        method: "POST",
+        body: JSON.stringify({ refresh: refreshToken }),
+    });
+}
+
+// Jobs
+
+export const getJobs = () => request("api/jobs/");
+
+export const createJob = (jobData) => {
+    request("api/jobs/", {
+        method: "POST",
+        body: JSON.stringify(jobData),
+    })
+}
+
+export const deleteJob = (jobId) => {
+    request(`api/jobs/${jobId}/`, {
+        method: "DELETE",
+    })
+}
